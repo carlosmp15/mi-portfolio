170c463bb3df77bc0fb0238f27055a964d689421
Carlos Murillo <cmurillperez@gmail.com>
2025-11-01 13:08:38 +0100

    feat: Update .gitignore and configure Astro settings for site and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+    integrations: [tailwind()],
+    site: "https://github.com/carlosmp15/mi-portfolio",
+    base: "mi-portfolio",
 });
